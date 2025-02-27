6e1c52b1cc991a3a9423cbf2c83e06ca9629832c
ahmedmedht <ahmed.m.mehisen@gmail.com>
2025-02-27 07:05:06 +0200

    last update

--- src/components/sections/Contact.jsx
+++ src/components/sections/Contact.jsx
@@ -12,7 +12,7 @@ export const Contact = () => {
 
 
     const handleSubmit = (e) => {
-        e,preventDefault()
+        e.preventDefault()
 
         emailjs.sendForm(import.meta.env.VITE_SERVICE_ID, import.meta.env.VITE_TEMPLATE_ID, e.target, import.meta.env.VITE_PUBLIC_KEY).then((result) => {
             alert("Message Sent!")
--- src/components/sections/Projects.jsx
+++ src/components/sections/Projects.jsx
@@ -12,6 +12,27 @@ export const Projects = () => {
                         Featured Projects
                     </h2>
                     <div className="grid grid-cols-1 md:grid-cols-2 gap-6">
+                    <div className="p-6 rounded-xl border border-white/10 hover:-translate-y-1 hover:border-blue-500/30 
+                                            hover:shadow-[0_2px_8px_rgba(59,130,246,0.2)] transition">
+                            <h3 className="text-xl font-bold mb-2">Ahmed Mehisen Portfolio</h3>
+                            <p className="text-gray-400 mb-4">A sleek and responsive personal portfolio built with React, Vite, and TailwindCSS,
+                                 featuring modern design, smooth animations, and interactive UI components. Fully optimized for performance and customization.</p>
+                            <div>
+                                {["React", "JavaScript", "Tailwindcss", "Vite"].map((tech, key) => (
+                                    <span key={key}
+                                        className="bg-blue-500/10 text-blue-500 py-1 px-3 rounded-full text-sm hover:bg-blue-500/20
+                                                    hover:shadow-[0_2px_8px_rgba(59,130,246,0.1)] transition-all">
+                                        {tech}
+                                    </span>
+                                ))}
+                            </div>
+
+                            <div className="flex justify-between items-center ">
+                                <a href="https://github.com/ahmedmedht/Personal-Portfolio"
+                                    className="text-blue-400 hover:text-blue-300 transition-colors"> View Project →</a>
+                            </div>
+                        </div>
+
                         <div className="p-6 rounded-xl border border-white/10 hover:-translate-y-1 hover:border-blue-500/30 
                                             hover:shadow-[0_2px_8px_rgba(59,130,246,0.2)] transition">
                             <h3 className="text-xl font-bold mb-2">Blog Api</h3>
